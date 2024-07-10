6f1da3919240700e480d687cf3c6dd2b9e2c5f60
Isaiah IM <46448947+isaiahIM@users.noreply.github.com>
2024-07-10 16:11:07 +0900

    Feat: html 페이지 타임리프 연결

---
+++ src/main/java/com/jolipjo/lovebridge/domain/HomeController.java
@@ -0,0 +1,16 @@
+package com.jolipjo.lovebridge.domain;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping
+public class HomeController {
+
+    @GetMapping
+    public String mainPage(Model model) {
+        return "html/index.html";
+    }
+}
