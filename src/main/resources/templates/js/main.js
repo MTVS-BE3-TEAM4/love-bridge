4985299dd96cb2414e28f398692a8d8914bb681e
Hoon <gns2862@naver.com>
2024-07-10 09:36:30 +0900

    Feat : 앨범, 메인 수정

--- src/main/resources/templates/js/main.js
+++ src/main/resources/templates/js/main.js
@@ -21,22 +21,45 @@
         tl.reverse();
     });
 
+    // gsap.registerPlugin(ScrollTrigger);
+
+    // const textElements = gsap.utils.toArray('.hover-box');
+
+    // textElements.forEach(text => {
+    //     gsap.to(text, {
+    //         backgroundSize: '100%',
+    //         ease: 'none',
+    //         scrollTrigger: {
+    //         trigger: text,
+    //         start: 'center 90%',
+    //         end: 'center 90%',
+    //         scrub: true,
+    //         },
+    //     });
+    // });
+
     gsap.registerPlugin(ScrollTrigger);
 
-    const textElements = gsap.utils.toArray('.hover-box');
+ScrollTrigger.defaults({
+  toggleActions: "play reset play reset",
+  markers: false
+});
 
-    textElements.forEach(text => {
-        gsap.to(text, {
-            backgroundSize: '100%',
-            ease: 'none',
-            scrollTrigger: {
-            trigger: text,
-            start: 'center 90%',
-            end: 'center 90%',
-            scrub: true,
-            },
-        });
-    });
+document.querySelectorAll(".text").forEach((text, index) => {
+  const targetID = text.dataset.target;
+  const target = document.querySelector(`#${targetID}`);
+  console.log(target)
+  gsap.to(text, {
+    autoAlpha: 1,
+    duration: 0.5,
+    // ease: "power4.inOut",
+    scrollTrigger: {
+      trigger: target,
+      start: "top center",
+      end: "bottom center"
+    }
+  });
+});
 
 var sakura = new Sakura('.sakura-bg', {
     colors: [
@@ -59,4 +82,3 @@ var sakura = new Sakura('.sakura-bg', {
     delay: 200
 });
 
-!function(e,t,a){function n(){c(".heart{width: 11%;height: 11%;position: fixed;background: #f00;transform: rotate(45deg);-webkit-transform: rotate(45deg);-moz-transform: rotate(45deg);}.heart:after,.heart:before{content: '';width: inherit;height: inherit;background: inherit;border-radius: 50%;-webkit-border-radius: 50%;-moz-border-radius: 50%;position: fixed;}.heart:after{top: -5px;}.heart:before{left: -5px;}"),o(),r()}function r(){for(var e=0;e<d.length;e++)d[e].alpha<=0?(t.body.removeChild(d[e].el),d.splice(e,1)):(d[e].y--,d[e].scale+=.004,d[e].alpha-=.013,d[e].el.style.cssText="left:"+d[e].x+"px;top:"+d[e].y+"px;opacity:"+d[e].alpha+";transform:scale("+d[e].scale+","+d[e].scale+") rotate(45deg);background:"+d[e].color+";z-index:99999");requestAnimationFrame(r)}function o(){var t="function"==typeof e.onclick&&e.onclick;e.onclick=function(e){t&&t(),i(e)}}function i(e){var a=t.createElement("div");a.className="heart",d.push({el:a,x:e.clientX-5,y:e.clientY-5,scale:1,alpha:1,color:s()}),t.body.appendChild(a)}function c(e){var a=t.createElement("style");a.type="text/css";try{a.appendChild(t.createTextNode(e))}catch(t){a.styleSheet.cssText=e}t.getElementsByTagName("head")[0].appendChild(a)}function s(){return"rgb("+~~(255*Math.random())+","+~~(255*Math.random())+","+~~(255*Math.random())+")"}var d=[];e.requestAnimationFrame=function(){return e.requestAnimationFrame||e.webkitRequestAnimationFrame||e.mozRequestAnimationFrame||e.oRequestAnimationFrame||e.msRequestAnimationFrame||function(e){setTimeout(e,1e3/60)}}(),n()}(window,document);
