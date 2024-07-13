7d92b56ee08ae7114f7b6a420eb3f42060414876
Isaiah IM <46448947+isaiahIM@users.noreply.github.com>
2024-07-13 14:21:09 +0900

    Feat: [Member] DTO 제작

---
+++ src/main/java/com/jolipjo/lovebridge/domain/member/dto/FindEmailRequestDTO.java
@@ -0,0 +1,13 @@
+package com.jolipjo.lovebridge.domain.member.dto;
+
+public class FindEmailRequestDTO {
+    private String email;// 이메일
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
---
+++ src/main/java/com/jolipjo/lovebridge/domain/member/dto/FindPasswordRequestDTO.java
@@ -0,0 +1,13 @@
+package com.jolipjo.lovebridge.domain.member.dto;
+
+public class FindPasswordRequestDTO {
+    private String email;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
