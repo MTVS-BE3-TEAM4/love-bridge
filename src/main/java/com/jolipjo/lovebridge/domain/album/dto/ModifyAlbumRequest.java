b3fb3ec9f711953330c6f483ce72f1e6da34d072
영민 <thsdudals208@naver.com>
2024-07-15 10:31:54 +0900

    Feat: 앨범 메인 수정 작성 DTO
    AlbumResponse
    ModifyAlbumRequest
    WriteAlbumRequest
    DTO작성

---
+++ src/main/java/com/jolipjo/lovebridge/domain/album/dto/ModifyAlbumRequest.java
@@ -0,0 +1,20 @@
+package com.jolipjo.lovebridge.domain.album.dto;
+
+public class ModifyAlbumRequest {
+
+    private String image;
+    private String memo;
+    private int date;
+
+    public String getImage() {return image;}
+
+    public void setImage(String image) {this.image = image;}
+
+    public String getMemo() {return memo;}
+
+    public void setMemo(String memo) {this.memo = memo;}
+
+    public int getDate() {return date;}
+
+    public void setDate(int date) {this.date = date;}
+}
