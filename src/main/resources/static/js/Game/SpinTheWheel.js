845b5151d2767fcea5204ffea671d37e2522dd09
hyahn <licors999@gmail.com>
2024-07-20 21:40:00 +0900

    룰렛 주요기능 완성 (CSS 는 아직 미비함)
    - 회전 후 그림과 alert 결과 일치하도록 수정
    - 회전 시 돌림판 위치 초기화하여 각도 틀어지는 상황 방지

--- src/main/resources/static/js/Game/SpinTheWheel.js
+++ src/main/resources/static/js/Game/SpinTheWheel.js
@@ -7,26 +7,33 @@
     5. 결과 (숫자에 따라 각도 조절 계산식 어떻게?)
     6. 한 번 돌리고 다시 돌리려면 각도 리셋
  */
-let numberOfBetItems = 4;
-
-// 룰렛에 들어갈 항목
-let betItems = [
-];
 
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
 
 // 각 항목에 해당하는 색상
 const colors = [
-  "#F89E97", "#F0A84A", "#F7D24A", "#B4DA66",
+    "#F89E97", "#F0A84A", "#F7D24A", "#B4DA66",
     "#9FC2E7", "#C2A0DA", "#B4E0E8", "#F3F3F3"
 ];
+
+// 룰렛에 들어갈 항목
+let betItems = [];
+
+// 항목 수가 먼저 정해지므로 변수 추가함
+let numberOfBetItems = 4;
+
+const clearCanvas = () => {
+    canvas.style.transform = `initial`;
+    canvas.style.transition = `initial`;
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+};
 
 // 돌림판과 문자입력 창 그리기
 function updateNumberDisplay() {
     document.getElementById('number').textContent = numberOfBetItems;
     generateInputText()
-    newMake()
+    makeWheel()
 }
 
 // 증가 버튼 클릭 이벤트 리스너
@@ -45,9 +52,8 @@ document.getElementById('decrement').addEventListener('click', () => {
     }
 });
 
-// 설정완료 버튼 동작, 돌림판에 그렸던 텍스트 지우고 새로 입력된 내용 반영함.
+// 설정완료 버튼 동작, 돌림판에 그렸던 텍스트들을 지우고 새로 입력된 내용 반영함.
 const setInputBetItems = () => {
-
     betItems = [];
     for (let i = 0; i < numberOfBetItems; i++) {
         const optionValue = document.getElementById(`optionInput${i}`).value;
@@ -58,15 +64,16 @@ const setInputBetItems = () => {
         betItems.push(optionValue);
     }
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    newMake();
+    makeWheel();
     changeShowButton();
 }
 
+//재설정하기 버튼 동작
 const reset = () => {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     changeShowButton();
     betItems = []
-    newMake();
+    makeWheel();
 }
 
 const changeShowButton = () => {
@@ -90,22 +97,20 @@ const changeShowButton = () => {
 }
 
 const rotate = () => {
-  canvas.style.transform = `initial`;
-  canvas.style.transition = `initial`;
-  
-  setTimeout(() => {
-    
-    const ran = Math.floor(Math.random() * betItems.length);
+    clearCanvas()
+    makeWheel();
+    setTimeout(() => {
 
-    const arc = 360 / betItems.length;
-    const rotate = (ran * arc) + 1800 + (arc / 9);
-    console.log(arc, rotate, ran);
-    
-    canvas.style.transform = `rotate(${rotate}deg)`;
-    canvas.style.transition = `2s`;
-    
-    setTimeout(() => alert(`${betItems[ran]} 선택되었습니다.`), 2000);
-  }, 2);
+        const ran = Math.floor(Math.random() * betItems.length);
+        const arc = 360 / betItems.length;
+        const rotate = (ran * arc) + 1800 + 95 + Math.floor(Math.random() * (arc - 5));
+        console.log(arc, rotate, ran);
+
+        canvas.style.transform = `rotate(-${rotate}deg)`;
+        canvas.style.transition = `2s`;
+
+        setTimeout(() => alert(`${betItems[ran]} 선택되었습니다.`), 2000);
+    }, 700);
 };
 
 // numberOfBetItems 숫자에 따라 텍스트 입력창 생성
@@ -124,10 +129,10 @@ function generateInputText() {
 }
 
 // 돌림판 생성
-const newMake = () => {
+const makeWheel = () => {
     const [cw, ch] = [canvas.width / 2, canvas.height / 2];
     const arc = Math.PI * (2 / numberOfBetItems);
-    ctx.remove();
+
     // 돌림판 배경
     for (let i = 0; i < numberOfBetItems; i++) {
         ctx.beginPath();
@@ -145,13 +150,14 @@ const newMake = () => {
     for (let i = 0; i < betItems.length; i++) {
         const angle = (arc * i) + (arc / 2);
 
-        ctx.save()  ;
+        ctx.save();
 
         ctx.translate(
             cw + Math.cos(angle) * (cw - 50),
             ch + Math.sin(angle) * (ch - 50),
         );
 
+        // 글자 회전, 기획에서 회전시키자고 하면 추가할 예정
         // ctx.rotate(angle + Math.PI / 2);
 
         betItems[i].split(" ").forEach((text, j) => {
